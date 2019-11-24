656d955367cf3952045bcc71d41ab012938c425c
Yomna Mahmoud Hussien | Ejada Systems Ltd <Yhussien@ejadacairo.local>
2019-11-24 16:16:04 +0200

    regex match

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -24,59 +24,76 @@ export class HomePage {
     // .then((res: string) => {
 
     this.camera.getPicture().then((res) => {
+      console.log(res)
       this.ocr.recText(OCRSourceType.NORMFILEURL, res)
-    // this.ocr.recText(OCRSourceType.NORMFILEURL, "file:///storage/emulated/0/1998CAM/ehabpass.jpg")
+    // this.ocr.recText(OCRSourceType.NORMFILEURL, "file:///storage/emulated/0/1998CAM/gamal.jpg")
     .then((res: OCRResult) => {
+      console.log(res)
       let data = '';
-      let i = res['lines']['linetext'].length;
+      this.passNo = '';
+      this.countryCode = '';
+      this.birthDay = '';
+      this.expiryDate = '';
+      this.nat = '';
+      // let i = res['lines']['linetext'].length;
       res['lines']['linetext'].forEach(element => {
-          if(res['lines']['linetext'][i-1]) {
-          data = res['lines']['linetext'][i-1]
-          console.log("--------------------------",data)
+          // if(res['lines']['linetext'][i-1]) {
+          // data = res['lines']['linetext'][i-1]
+          // console.log("--------------------------",data)
+        // }
+        element = element.replace(/\s/g,'')
+        const line = element.match(/.{10}[A-Za-z]{3}[0-9]{7}[A-Za-z]{1}[0-9]{7}/gi)
+        if (line) {
+          data = line[0];
         }
       });
-      data = data.replace(/\s/g,'')
-      let passportNum = data.substr(0,9);
-      passportNum = passportNum.replace("<<","");
-      this.passNo = passportNum;
-      const country = data.match(/[A-Za-z]{3}/gi)[0];
-      this.countryCode = country;
-      let dob = data.match(/[A-Za-z]{3}[0-9]{6}/gi)[0].substr(3)
-      let expdate = data.match(/[0-9][A-Za-z][0-9]{6}/gi)[0].substr(2)
-      
-      if (+dob.substr(0, 2) > (new Date()).getFullYear() - 2000){
-        dob = dob.substr(4,2) + "/" + dob.substr(2,2) + "/" + "19" + dob.substr(0, 2)
+      if (data) {
+        console.log(data)
+        let passportNum = data.substr(0,9);
+        console.log(passportNum)
+        passportNum = passportNum.replace("<<","");
+        this.passNo = passportNum;
+        const country = data.match(/[A-Za-z]{3}/gi)[0];
+        this.countryCode = country;
+        let dob = data.match(/[A-Za-z]{3}[0-9]{6}/gi)[0].substr(3)
+        let expdate = data.match(/[0-9][A-Za-z][0-9]{6}/gi)[0].substr(2)
+        
+        if (+dob.substr(0, 2) > (new Date()).getFullYear() - 2000){
+          dob = dob.substr(4,2) + "/" + dob.substr(2,2) + "/" + "19" + dob.substr(0, 2)
+        } else {
+          dob =  dob.substr(4,2) + "/" + dob.substr(2,2) + "/" + "20" + dob.substr(0, 2)
+        }
+        this.birthDay = dob;
+  
+        expdate =  expdate.substr(4,2) + "/" + expdate.substr(2,2) + "/" + "20" + expdate.substr(0, 2)
+        this.expiryDate = expdate;
+  
+        let nationality = "Egyptian";
+        if(country == "USA") {
+          nationality = "American";
+        }
+        else if(country == "HRV") {
+          nationality = "Croatian";
+        }
+        else if(country == "LVA") {
+          nationality = "Latvian";
+        }
+        else if(country == "CHN" || country == "cHN") {
+          nationality = "Chineese";
+        }
+        else if(country == "SAU") {
+          nationality = "Saudi Arabian"
+        }
+        this.nat = nationality;
+  
+        console.log("Passport Num:", passportNum)
+        console.log("Country Code: ", country)
+        console.log("Date of birth: ", dob)
+        console.log("Expiary Date: ", expdate)
+        console.log("Nationality:", nationality)
       } else {
-        dob =  dob.substr(4,2) + "/" + dob.substr(2,2) + "/" + "20" + dob.substr(0, 2)
+        alert('Please retake the picture')
       }
-      this.birthDay = dob;
-
-      expdate =  expdate.substr(4,2) + "/" + expdate.substr(2,2) + "/" + "20" + expdate.substr(0, 2)
-      this.expiryDate = expdate;
-
-      let nationality = "Egyptian";
-      if(country == "USA") {
-        nationality = "American";
-      }
-      else if(country == "HRV") {
-        nationality = "Croatian";
-      }
-      else if(country == "LVA") {
-        nationality = "Latvian";
-      }
-      else if(country == "CHN" || country == "cHN") {
-        nationality = "Chineese";
-      }
-      else if(country == "SAU") {
-        nationality = "Saudi Arabian"
-      }
-      this.nat = nationality;
-
-      console.log("Passport Num:", passportNum)
-      console.log("Country Code: ", country)
-      console.log("Date of birth: ", dob)
-      console.log("Expiary Date: ", expdate)
-      console.log("Nationality:", nationality)
     })
     .catch((error: any) => console.error(error));
   // })
