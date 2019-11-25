e1af2a1a9b749091ed2c9be1a04c1bb4274fafdb
Yomna Mahmoud Hussien | Ejada Systems Ltd <Yhussien@ejadacairo.local>
2019-11-25 12:10:18 +0200

    fixing code with TSLink

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,102 +11,93 @@ import { Camera } from '@ionic-native/camera/ngx';
 export class HomePage {
   selectedImage: string;
   word: string[];
-  nat = "";
-  passNo = "";
-  expiryDate = "";
-  birthDay = "";
-  countryCode = "";
-  constructor(private ocr: OCR, private camera: Camera) {}
+  nat = '';
+  passNo = '';
+  expiryDate = '';
+  birthDay = '';
+  countryCode = '';
+  constructor(private ocr: OCR, private camera: Camera) { }
 
   onPickImage() {
     // let opts: DocumentScannerOptions = {};
     //  DocumentScanner.scanDoc(opts)
     // .then((res: string) => {
 
-    var cameraOptions = {
+    const cameraOptions = {
       encodingType: this.camera.EncodingType.JPEG,
       correctOrientation: true
     };
 
     this.camera.getPicture(cameraOptions).then((res) => {
-      console.log(res)
-      this.ocr.recText(OCRSourceType.NORMFILEURL, res)
-    // this.ocr.recText(OCRSourceType.NORMFILEURL, "file:///storage/emulated/0/1998CAM/gamal.jpg")
-    .then((res: OCRResult) => {
-      console.log( res['lines']['linetext'])
-      let data = '';
-      this.passNo = '';
-      this.countryCode = '';
-      this.birthDay = '';
-      this.expiryDate = '';
-      this.nat = '';
-      // let i = res['lines']['linetext'].length;
-      res['lines']['linetext'].forEach(element => {
+      console.log(res);
+      const firstParam = 'lines';
+      const secondParam = 'linetext';
+      this.ocr.recText(OCRSourceType.NORMFILEURL, res).then((res2: OCRResult) => {
+        console.log(res2[firstParam][secondParam]);
+        let data = '';
+        this.passNo = '';
+        this.countryCode = '';
+        this.birthDay = '';
+        this.expiryDate = '';
+        this.nat = '';
+        // let i = res['lines']['linetext'].length;
+        res2[firstParam][secondParam].forEach(element => {
           // if(res['lines']['linetext'][i-1]) {
           // data = res['lines']['linetext'][i-1]
           // console.log("--------------------------",data)
-        // }
-        element = element.replace(/\s/g,'')
-        const line = element.match(/.{10}[A-Za-z]{3}[0-9]{7}[A-Za-z]{1}[0-9]{7}/gi)
-        if (line) {
-          data = line[0];
-        }
-      });
-      if (data) {
-        console.log(data)
-        let passportNum = data.substr(0,9);
-        console.log(passportNum)
-        passportNum = passportNum.replace("<<","");
-        this.passNo = passportNum;
-        const country = data.match(/[A-Za-z]{3}/gi)[0];
-        this.countryCode = country;
-        let dob = data.match(/[A-Za-z]{3}[0-9]{6}/gi)[0].substr(3)
-        let expdate = data.match(/[0-9][A-Za-z][0-9]{6}/gi)[0].substr(2)
-        
-        if (+dob.substr(0, 2) > (new Date()).getFullYear() - 2000){
-          dob = dob.substr(4,2) + "/" + dob.substr(2,2) + "/" + "19" + dob.substr(0, 2)
+          // }
+          element = element.replace(/\s/g, '');
+          const line = element.match(/.{10}[A-Za-z]{3}[0-9]{7}[A-Za-z]{1}[0-9]{7}/gi);
+          if (line) {
+            data = line[0];
+          }
+        });
+        if (data) {
+          console.log(data);
+          let passportNum = data.substr(0, 9);
+          console.log(passportNum);
+          passportNum = passportNum.replace('<<', '');
+          this.passNo = passportNum;
+          const country = data.match(/[A-Za-z]{3}/gi)[0];
+          this.countryCode = country;
+          let dob = data.match(/[A-Za-z]{3}[0-9]{6}/gi)[0].substr(3);
+          let expdate = data.match(/[0-9][A-Za-z][0-9]{6}/gi)[0].substr(2);
+          if (+dob.substr(0, 2) > (new Date()).getFullYear() - 2000) {
+            dob = dob.substr(4, 2) + '/' + dob.substr(2, 2) + '/' + '19' + dob.substr(0, 2);
+          } else {
+            dob = dob.substr(4, 2) + '/' + dob.substr(2, 2) + '/' + '20' + dob.substr(0, 2);
+          }
+          this.birthDay = dob;
+          expdate = expdate.substr(4, 2) + '/' + expdate.substr(2, 2) + '/' + '20' + expdate.substr(0, 2);
+          this.expiryDate = expdate;
+          let nationality = 'Egyptian';
+          if (country === 'USA') {
+            nationality = 'American';
+          } else if (country === 'HRV') {
+            nationality = 'Croatian';
+          } else if (country === 'LVA') {
+            nationality = 'Latvian';
+          } else if (country === 'CHN' || country === 'cHN') {
+            nationality = 'Chineese';
+          } else if (country === 'SAU') {
+            nationality = 'Saudi Arabian';
+          }
+          this.nat = nationality;
+          console.log('Passport Num: ', passportNum);
+          console.log('Country Code: ', country);
+          console.log('Date of birth: ', dob);
+          console.log('Expiary Date: ', expdate);
+          console.log('Nationality:', nationality);
         } else {
-          dob =  dob.substr(4,2) + "/" + dob.substr(2,2) + "/" + "20" + dob.substr(0, 2)
+          alert('Please retake the picture');
         }
-        this.birthDay = dob;
-  
-        expdate =  expdate.substr(4,2) + "/" + expdate.substr(2,2) + "/" + "20" + expdate.substr(0, 2)
-        this.expiryDate = expdate;
-  
-        let nationality = "Egyptian";
-        if(country == "USA") {
-          nationality = "American";
-        }
-        else if(country == "HRV") {
-          nationality = "Croatian";
-        }
-        else if(country == "LVA") {
-          nationality = "Latvian";
-        }
-        else if(country == "CHN" || country == "cHN") {
-          nationality = "Chineese";
-        }
-        else if(country == "SAU") {
-          nationality = "Saudi Arabian"
-        }
-        this.nat = nationality;
-  
-        console.log("Passport Num:", passportNum)
-        console.log("Country Code: ", country)
-        console.log("Date of birth: ", dob)
-        console.log("Expiary Date: ", expdate)
-        console.log("Nationality:", nationality)
-      } else {
-        alert('Please retake the picture')
-      }
-    })
-    .catch((error: any) => console.error(error));
-  // })
+      })
+        .catch((error: any) => console.error(error));
+      // })
       // .catch((error: any) => console.error(error));
     }, (err) => {
       console.log(err);
-     });
+    });
 
-    }
-    
+  }
 }
